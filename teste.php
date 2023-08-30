501c0037e3e0a625f2aa9ea97e7a9ab8b3454704
TiagoUniverse <tiagocesar68@gmail.com>
2023-08-30 17:19:33 -0300

    consegui submeter o arquivo enviado para uma pasta do projeto

---
+++ teste.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="teste2.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="fileEscolaridade" accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps" required="">
+        <button type="submit"> enviar</button>
+    </form>
+</body>
+
+</html>
---
+++ teste2.php
@@ -0,0 +1,34 @@
+<?php
+
+$diretorio='docs/';
+
+$temp = explode('.', $_FILES['fileEscolaridade']['name']);
+
+$extensao = end($temp);
+
+$nome = $_FILES['fileEscolaridade']['name'];
+
+echo "Nome do arquivo:";
+for($contador = 0; $contador < count($temp) -1; $contador++){
+    echo $temp[$contador];
+}
+
+echo "<br>";
+$novoNome = time() . '.' . $extensao;
+
+echo $novoNome;
+
+move_uploaded_file($_FILES['fileEscolaridade']["tmp_name"] , $diretorio . $novoNome);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste 2</title>
+</head>
+<body>
+    <h1>Arquivo salvo!</h1>
+</body>
+</html>
